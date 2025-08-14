9ef56c1d392765790c67537af3e8ee42dbbf8b34
eki <cryptoeki22@gmail.com>
2025-08-14 10:22:49 +0800

    Fix Gradle configuration for Android build compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,16 +7,16 @@ plugins {
 
 android {
     namespace = "com.example.asisten_guru"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    compileSdk = 34
+    ndkVersion = "25.1.8937393"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
@@ -24,10 +24,10 @@ android {
         applicationId = "com.example.asisten_guru"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
